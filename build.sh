adea7ca85f02f3e4d895e4bc982c87b9f6914584
durcoke <durcor@disroot.org>
2019-11-05 16:46:31 -0500

    More images, release build script fixed

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ version=$1
 mkdir releases/$version
 rm -rf releases/$version/*
 cp *.html releases/$version/
+cp *.css releases/$version/
 cp -r images releases/$version/
 rm -rf releases/$version/images/src
 # cp report.md.pdf releases/$version/report.pdf
