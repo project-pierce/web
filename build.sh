b3507a1246d33fb22c64e8acddbcaf86c7237c6c
durcoke <durcor@disroot.org>
2019-11-05 11:33:10 -0500

    Modified build file, added pledge

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ mkdir releases/$version
 rm -rf releases/$version/*
 cp *.html releases/$version/
 cp -r images releases/$version/
-cp report.md.pdf releases/$version/report.pdf
+rm -rf releases/$version/images/src
+# cp report.md.pdf releases/$version/report.pdf
 cd releases/$version
 7z a $1.zip *
