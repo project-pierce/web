ed52675496d93b893086bdec96a69d47cb59d43e
durcoke <durcor@disroot.org>
2019-12-04 15:30:37 -0500

    Finishing up JS

--- recipe-maker.js
+++ recipe-maker.js
@@ -9,20 +9,7 @@ function showImage(event) {
     previewImage.src = URL.createObjectURL(event.target.files[0]);
 };
 
-function saveData() {
-    document.cookie="fname="+document.getElementById('fname').value+"; expires=Thu, 18 Dec 2019 12:00:00 UTC";
-    document.cookie="email="+document.getElementById('email').value+"; expires=Thu, 18 Dec 2019 12:00:00 UTC";
-
-    var radios = document.getElementsByName('gender');
-
-    for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            document.cookie="gender="+radios[i].value+"; expires=Thu, 18 Dec 2019 12:00:00 UTC";
-            break;
-        }
-    }
-
-    alert('Data Saved!');
+function displayNewRecipe() {
+    document.getElementById('display').innerHTML = document.getElementById('meal').value;
+    // document.getElementById('display').innerHTML = document.getElementById('meal').value;
 }
-
-// alert('SAVED DATA: '+document.cookie);
