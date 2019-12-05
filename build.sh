21ea7464cfbc1b389d356d19e9a1fc31fbf057b5
durcoke <durcor@disroot.org>
2019-12-05 09:39:06 -0500

    More recipes

--- build.sh
+++ build.sh
@@ -10,7 +10,6 @@ cp *.css releases/$version/Team09
 cp *.js releases/$version/Team09
 cp -r images releases/$version/Team09
 cp -r videos releases/$version/Team09
-rm -rf releases/$version/Team09/images/src
 cp pres.pptx releases/$version/
 # cp report-$version.md.pdf releases/$version/report.pdf
 cd releases/$version
